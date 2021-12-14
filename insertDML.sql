3e2e6b31d6a2c847bb82c8f502abf0fc120d0324
Rutuja <73716522+rj-rutu@users.noreply.github.com>
2021-12-14 11:59:21 +0530

    Update insertDML.sql

--- insertDML.sql
+++ insertDML.sql
@@ -3,6 +3,7 @@ VALUES
 ('ramesh@gmail.com','password13','Ramesh','Tamil Nadu', 'male'),
 ('sahil@gmail.com','password13','Sahil','Karnataka', 'male'),
 ('tanaya@gmail.com','password13','Tanaya','Gujarat', 'female')
+select* from table_name
 ;
 
 INSERT INTO MedicalHistory(id,date,conditions,surgeries,medication)
@@ -10,12 +11,14 @@ VALUES
 (1,'19-01-14','Pain in abdomen','Heart Surgery','Crocin'),
 (2,'19-01-14','Frequent Indigestion','none','none'),
 (3,'19-01-14','Body Pain','none','Iodex')
+select* from table_name
 ;
 
 INSERT INTO Doctor(email, gender, password, name)
 VALUES
 ('hathalye7@gmail.com', 'female', 'hrishikesh13', 'Priyanka Manglulkar'),
 ('hathalye8@gmail.com', 'female', 'hrishikesh13', 'Priyanka Manglulkar')
+select* from table_name
 ;
 
 INSERT INTO Appointment(id,date,starttime,endtime,status)
@@ -23,6 +26,7 @@ VALUES
 (1, '19-01-15', '09:00', '10:00', 'Done'),
 (2, '19-01-16', '10:00', '11:00', 'Done'),
 (3, '19-01-18', '14:00', '15:00', 'Done')
+select* from table_name
 ;
 
 INSERT INTO PatientsAttendAppointments(patient,appt,concerns,symptoms)
@@ -30,6 +34,7 @@ VALUES
 ('ramesh@gmail.com',1, 'none', 'itchy throat'),
 ('sahil@gmail.com',2, 'infection', 'fever'),
 ('tanaya@gmail.com',3, 'nausea', 'fever')
+select* from table_name
 ;
 
 INSERT INTO Schedule(id,starttime,endtime,breaktime,day)
@@ -40,6 +45,7 @@ VALUES
 (001,'09:00','17:00','12:00','Sunday'),
 (002,'09:00','17:00','12:00','Wednesday'),
 (002,'09:00','17:00','12:00','Friday')
+select* from table_name
 ;
 
 INSERT INTO PatientsFillHistory(patient,history)
@@ -47,6 +53,7 @@ VALUES
 ('ramesh@gmail.com', 1),
 ('sahil@gmail.com', 2),
 ('tanaya@gmail.com', 3)
+select* from table_name
 ;
 
 INSERT INTO Diagnose(appt,doctor,diagnosis,prescription)
@@ -54,12 +61,14 @@ VALUES
 (1,'hathalye7@gmail.com', 'Bloating', 'Ibuprofen as needed'),
 (2,'hathalye8@gmail.com', 'Muscle soreness', 'Stretch morning/night'),
 (3,'hathalye8@gmail.com', 'Vitamin Deficiency', 'Good Diet')
+select* from table_name
 ;
 
 INSERT INTO DocsHaveSchedules(sched,doctor)
 VALUES
 (001,'hathalye7@gmail.com'),
 (002,'hathalye8@gmail.com')
+select* from table_name
 ;
 
 INSERT INTO DoctorViewsHistory(history,doctor)
@@ -67,4 +76,5 @@ VALUES
 (1,'hathalye7@gmail.com'),
 (2,'hathalye8@gmail.com'),
 (3,'hathalye8@gmail.com')
+select* from table_name
 ;
